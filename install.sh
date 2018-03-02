91a0de37a00a82a7f3abf9fbac2bdd64ca3c17a9
Justin Dang <justin@justind.me>
2018-03-03 01:34:22 +1100

    Add main script and install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+mv ./unixcommand /usr/local/bin/
+sudo chmod +x /usr/local/bin/unixcommand
+touch /etc/glossary.json
+
+echo 'Install completed'
+echo 'You may delete this directory' 
